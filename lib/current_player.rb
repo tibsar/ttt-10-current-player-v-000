9a3c368e279051176ce0f03937e821c1cbbc67d3
Sara Tibbetts <sxt4866@rit.edu>
2018-03-06 02:35:42 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,10 +1,12 @@
 # board = ["O", " ", " ", " ", "X", " ", " ", " ", "X"]
 
 def turn_count(board)
-  
+  count = 0 
   board.each do |item|
     if item == "X" || item == "O"
-      
+      count += 1 
     end 
   end 
+  
+  count 
 end 
